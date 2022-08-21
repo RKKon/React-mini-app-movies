ace89821a7151f3aaa269cf60306cf6969a3eca7
RKKon <rkkon@mail.ru>
2022-08-21 20:37:29 +0300

    hangle bug to sing homepage

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <nav className="green darken-1">
       <div className="nav-wrapper">
-        <a href="/" className="brand-logo">React movies and TV show</a>
+        <a href="#!" className="brand-logo">React movies and TV show</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a target='_blank' rel="noreferrer" href="https://github.com/RKKon/React-app-mini-store/tree/main">Repository</a></li>
         </ul>
